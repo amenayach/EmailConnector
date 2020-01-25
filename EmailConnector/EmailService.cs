4bfa37fe1b0c5a6fe67ff1256d48d5d3b9e24ca7
Amen Ayach <aayach@Netways.com>
2020-01-26 00:19:32 +0200

    Initial Commit

---
+++ EmailConnector/EmailService.cs
@@ -0,0 +1,97 @@
+﻿namespace EmailConnector
+{
+    using System;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Mail;
+
+    /// <summary>
+    /// Disposable Class used to Send Emails using SMTP Server.
+    /// </summary>
+    public class EmailService : IDisposable
+    {
+        private readonly SmtpClient client;
+        private readonly string sender;
+
+        public EmailService(string host, int port, string sender, string username, string password, bool enableSsl)
+        {
+            this.sender = sender;
+
+            this.client = new SmtpClient(host, port)
+            {
+                UseDefaultCredentials = false,
+                EnableSsl = enableSsl,
+                Credentials = new NetworkCredential(username, password)
+            };
+        }
+
+        /// <summary>
+        /// Async method to Send Mail Message to one or many recipients.
+        /// </summary>
+        /// <param name="subject">subject of the email.</param>
+        /// <param name="body">html body string of the email.</param>
+        /// <param name="recipients">list of recipients email addresses</param>
+        public void SendEmail(string subject, string body, params string[] recipients)
+        {
+            if (string.IsNullOrWhiteSpace(subject))
+            {
+                throw new ArgumentException(nameof(subject), "value cannot be null or empty");
+            }
+
+            if (string.IsNullOrWhiteSpace(body))
+            {
+                throw new ArgumentException(nameof(body), "value cannot be null or empty");
+            }
+
+            if (recipients == null || recipients.Length == 0 || !recipients.Any(m => !string.IsNullOrWhiteSpace(m)))
+            {
+                throw new ArgumentException("Email should contain at least 1 valid recipient");
+            }
+
+            using (MailMessage mailMessage = new MailMessage())
+            {
+                mailMessage.From = new MailAddress(sender, "No Reply");
+                mailMessage.Sender = mailMessage.From;
+                mailMessage.Body = body;
+                mailMessage.Subject = subject;
+
+                mailMessage.IsBodyHtml = true;
+
+                foreach (var recipient in recipients)
+                {
+                    if (!string.IsNullOrWhiteSpace(recipient))
+                    {
+                        mailMessage.To.Add(recipient);
+                    }
+                }
+
+                client.SendMailAsync(mailMessage).Wait();
+            }
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    if (client != null)
+                    {
+                        client.Dispose();
+                    }
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+    }
+}
---
+++ EmailConnector/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace EmailConnector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter email server host or IP:");
+                var host = Console.ReadLine();
+
+                Console.WriteLine("Please enter the port:");
+                var port = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter the sender:");
+                var sender = Console.ReadLine();
+
+                Console.WriteLine("Please enter the username:");
+                var username = Console.ReadLine();
+
+                Console.WriteLine("Please enter the password:");
+                var password = Console.ReadLine();
+
+                Console.WriteLine("Please enter the enable SSL yes/no:");
+                var enableSsl = Console.ReadLine().ToLower() == "yes";
+
+                Console.WriteLine("Please enter the receiver's email:");
+                var email = Console.ReadLine();
+
+                var emailService = new EmailService(host, port, sender, username, password, enableSsl);
+
+                emailService.SendEmail("Test subject", $"Test body at {DateTime.Now:yyyy-MM-dd-HH-mm-ss}", email);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                if (ex.InnerException != null)
+                {
+                    Console.WriteLine(ex.InnerException.Message);
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Please hit enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
